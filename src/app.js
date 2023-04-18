0d11ca21e9e3f722f3c13887a4ad4e2eba28a59b
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2023-04-18 23:45:27 +0000

    modify app.js

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+/* ----------------------------- PROJECT -------------------*/
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k]);
+    }
+  }
+}
