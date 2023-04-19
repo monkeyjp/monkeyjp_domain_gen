7ff61176fad49b2ffba30e20925dd6f5e2f3e3a9
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2023-04-19 00:05:19 +0000

    add extentions array

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
 
 /* ----------------------------- PROJECT -------------------*/
 
 var pronoun = ["the", "our"];
 var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
+var extensions = [".com", ".net", ".us", ".io"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-      console.log(pronoun[i] + adj[j] + noun[k]);
+      for (let l = 0; l < extensions.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extensions[l]);
+      }
     }
   }
 }
